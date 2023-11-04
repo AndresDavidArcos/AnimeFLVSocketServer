59516bd8f4e7e09ef8611b0936e6ac34a23c58d4
AndresDavidArcos <andres.camargo@correounivalle.edu.co>
2023-11-04 15:38:04 -0500

    http added and socket modified

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-const Room = require('./classes/Room')
-const express = require('express')
-const app = express()
-const socketio = require('socket.io')
+const Room = require('./classes/Room');
+const express = require('express');
+const app = express();
 const cors = require("cors");
+const socketio = require('socket.io')
+
 const corsOptions = {
-  origin: '*', // Acepta solicitudes de cualquier origen
+  origin: '*', // Allow requests from any origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-const expresServer = app.listen(9000);
-const io = socketio(expresServer, {
+
+app.use(express.json());
+
+const port = 9000;
+const rooms = {};
+rooms['maraton de one piece'] = new Room("UUID-GENERATED-STRING", "maraton de one piece", "female", "zoro", "https://www3.animeflv.net/ver/rurouni-kenshin-meiji-kenkaku-romantan-2023-17", "SW", true, 5);
+
+app.get('/rooms', (req, res) => {
+  res.status(200).send(rooms);
+});
+
+app.post('/createRoom', (req, res) => {
+    const roomInfo = req.body;
+    const roomCreated = Room.createRoomFromJSON(roomInfo);
+    rooms[roomCreated.roomId] = roomCreated;
+    res.status(200).send({ message: "Room creada exitosamente"});
+  });
+
+
+  
+const expressServer = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+const io = socketio(expressServer, {
   cors: {
-    origin: '*', // Acepta conexiones de cualquier origen
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
-const rooms = {};
 
-rooms['maraton de one piece'] = new Room("UUID-GENERATED-STRING", "maraton de one piece", "female",  "zoro", "https://www3.animeflv.net/ver/rurouni-kenshin-meiji-kenkaku-romantan-2023-17", "SW",true, 5);
-
- io.on('connection',  (socket)=>{
+io.on('connection',  (socket)=>{
   console.log(socket.id, 'has connected');
 
-  socket.on('loginRequest', async(username, ackCallBack) => {
-          ackCallBack({type: "success", rooms});
-    })
-
-  socket.on("createRoomRequest", roomInfo => {
-    const roomCreated = Room.createRoomFromJSON(roomInfo);
-    rooms[roomCreated.roomId] = roomCreated;
-    //leave all rooms, because the host can only be in one room
-    const clientRooms = socket.rooms;
-    let i = 0;
-    clientRooms.forEach(room=>{
-    //we don't want to leave the socket's personal room which is guaranteed to be first
-    if(i!==0){
-      socket.leave(room);
-     }
-     i++;
-     })
-
-     socket.join(roomCreated.roomId);
-  })
-
   socket.on("joinRoom", async(roomInfo, sendResponse) => {
-    const thisRoom = rooms[roomInfo.roomId];
-    const thisRoomHistory = thisRoom.history;
-    
+    const thisRoom = rooms[roomInfo.roomId];    
     //leave all rooms, because the client can only be in one room
     const clientRooms = socket.rooms;
     let i = 0;
